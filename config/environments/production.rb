04d27e2f0aa7e30107c3298d533ee0fc926c2cc1
sara lai <shihualai@gmail.com>
2025-11-28 11:53:19 +0800

    heroku mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # for heroku until actually mailing
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = false
 end
